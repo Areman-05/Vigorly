fe72d6b51e8081cff76e555c579ef41c03a49dda
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2026-05-27 13:05:31 +0200

    test: add WeeklyProgressCalculator unit tests

---
+++ app/src/test/java/com/example/vigorly/WeeklyProgressCalculatorTest.kt
@@ -0,0 +1,21 @@
+package com.example.vigorly
+
+import com.example.vigorly.data.model.WeeklyGoal
+import com.example.vigorly.util.WeeklyProgressCalculator
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class WeeklyProgressCalculatorTest {
+
+    @Test
+    fun remainingSessions_neverNegative() {
+        val goal = WeeklyGoal(targetSessions = 5, completedSessions = 7)
+        assertEquals(0, WeeklyProgressCalculator.remainingSessions(goal))
+    }
+
+    @Test
+    fun percent_roundsDownProgress() {
+        val goal = WeeklyGoal(targetSessions = 8, completedSessions = 3)
+        assertEquals(37, WeeklyProgressCalculator.percent(goal))
+    }
+}
